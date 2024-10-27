56f117107633680d3a33ad9a0339eeff3e293c9c
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-26 18:00:27 -0600

    Crear 2 modulos countries y shared que contendra lo que se compartira a lo largo de la aplicacion como el sidebar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
