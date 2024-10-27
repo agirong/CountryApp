7a8f702fdd5e12860d5c37659a7132946a6e8b56
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-26 18:31:41 -0600

    Crear RouterModule y las Paginas about y home, en AppRoutingModule se definen los componentes que se van a mostrar según las rutas recibidas.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-
+    AppRoutingModule,
     SharedModule
   ],
   providers: [],
