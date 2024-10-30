081a16c6f0b479e8997fc33b2819c5cca658807d
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-10-29 23:49:43 -0600

    Mostrar Información del país. Faltan las traducciones.

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: ``
+  styles: [`
+
+  `]
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?:Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -22,11 +27,8 @@ export class CountryPageComponent implements OnInit {
       switchMap(({id})=> this.countriesService.searchCountryByAlphaCode(id)),
     )
     .subscribe(country=>{
-      if(!country){
-        this.router.navigateByUrl('');
-      }
-      console.log('Encontramos el pais')
-
+      if(!country) return this.router.navigateByUrl('');
+      return this.country = country;
     });
   }
 }
