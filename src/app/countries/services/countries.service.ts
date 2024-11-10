01a55fca5fa1a00f77413b66073a736a6355c7ee
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-11-10 16:43:11 -0600

    Refactor: Mejoras de las funcionalidades Sección 10. Se mejoró código del servicio de los llamados http.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of} from 'rxjs';
+import { catchError, delay, map, Observable, of} from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,14 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
   constructor(private http: HttpClient) { }
+
+  private getCountriesRequest (url:string): Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( ()=> of([])),
+      delay(2000),
+    );
+  }
 
   //Consulta para obtener la información del pais por codigo.
   searchCountryByAlphaCode(code:string): Observable<Country | null>{
@@ -22,26 +30,19 @@ export class CountriesService {
 
   //Consulta por nombre de la capital del país
   searchCapital(term: string): Observable<Country []>{
-    const url=`${this.apiUrl}/capital/${term}`
-    return this.http.get<Country[]>(url)
-    .pipe(
-      catchError( error => of([]))
-    );
+    const url=`${this.apiUrl}/capital/${term}`;
+    return this.getCountriesRequest(url);
   }
 
   //Consulta por el nombre common del país
   searchCountry(term: string):Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/name/${term}`)
-    .pipe(
-      catchError(()=> of([]))
-    )
+    const url = `${this.apiUrl}/name/${term}`;
+    return this.getCountriesRequest(url);
   }
 
   //Consulta por el continente
   searchRegion(term: string):Observable<Country[]>{
-    return this.http.get<Country[]>(`${this.apiUrl}/region/${term}`)
-    .pipe(
-      catchError(()=> of([]))
-    )
+    const url = `${this.apiUrl}/region/${term}`;
+    return this.getCountriesRequest(url);
   }
 }
