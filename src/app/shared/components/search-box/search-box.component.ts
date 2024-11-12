c03802350b6059749ce59e20a9e9b408897b1a1c
Aaron Obed Girón <aogarrido.g@gmail.com>
2024-11-11 20:38:26 -0600

    Limpiar suscripciones de Observable con metodo onDestroy de Angular, para ahorro de memoria.

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: ``
 })
-export class SearchBoxComponent implements OnInit {
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
   private debouncer:Subject<string> = new Subject<string>();
+  private debouncerSubscription?:Subscription;
 
   @Input()
   public placeholder:string = '';
@@ -20,13 +21,18 @@ export class SearchBoxComponent implements OnInit {
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSubscription = this.debouncer
     .pipe(
       debounceTime(300)
     )
     .subscribe(value =>{
       this.onDebounce.emit(value);
     })
+  }
+
+  //Se manda a llamar cuando el componente es destruido...
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
   }
 
   emitValue(value:string):void{
@@ -36,5 +42,4 @@ export class SearchBoxComponent implements OnInit {
   onKeyPress(searchTerm:string){
     this.debouncer.next(searchTerm)
   }
-
 }
